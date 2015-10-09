959d793d286a10c6fff4928407e1c9fb8cc7f911
patricksandquist <patricksandquist@gmail.com>
2015-10-09 11:44:45 -0700

    Add Bench store, action, and constants

---
+++ app/assets/javascripts/stores/bench.js
@@ -0,0 +1,19 @@
+(function(root){
+  var _benches = [];
+  var resetBenches = function (benches) {
+    _benches = benches;
+  };
+
+  root.BenchStore = $.extend({}, EventEmitter.prototype, {
+    all: function(){
+      return _benches.slice(0);
+    },
+
+    dispatcherID: AppDispatcher.register(function (payload) {
+      if (payload.actionType === BenchConstants.BENCHES_RECEIVED) {
+        resetBenches(payload.benches);
+      }
+    })
+  });
+
+})(this);

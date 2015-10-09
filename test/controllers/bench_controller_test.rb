3c7232502d8780984d5b9c3d0f723ae8d6442bcf
patricksandquist <patricksandquist@gmail.com>
2015-10-09 11:08:23 -0700

    Add Bench model and controller with JSON API

---
+++ test/controllers/bench_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class BenchControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+end

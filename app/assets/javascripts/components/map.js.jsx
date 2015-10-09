ae1a0486572949bb5bb07f8586ed01bb1a8142a3
Patrick Sandquist <patricksandquist@gmail.com>
2015-10-09 15:08:09 -0700

    Add markers of benches on the map

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -1,10 +1,24 @@
 window.Map = React.createClass({
+  _onChange: function () {
+    var _markers = [];
+    BenchStore.all().forEach(function (bench) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(bench.lat, bench.lng),
+        map: this.map,
+        title: bench.description
+      });
+      _markers.push(marker);
+    }.bind(this));
+  },
+
   componentDidMount: function () {
     var map = React.findDOMNode(this.refs.map);
     var mapOptions = {
       center: {lat: 37.7758, lng: -122.435},
       zoom: 12
     };
+
+    BenchStore.addChangeListener(this._onChange);
     this.map = new google.maps.Map(map, mapOptions);
   },
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Bench.create!(description: 'Golden Gate Park', lat: 37.4611, lng: 122.2918)
-Bench.create!(description: 'Lands End', lat: 37.4710, lng: 122.3020)
-Bench.create!(description: 'Presidio', lat: 37.4812, lng: 122.274637)
-Bench.create!(description: 'Fort Mason', lat: 37.4824, lng: 122.2536)
+Bench.create!(description: 'Golden Gate Park', lat:  37.769734, lng: -122.489770)
+Bench.create!(description: 'Lands End', lat: 37.781013, lng: -122.514145)
+Bench.create!(description: 'Presidio', lat: 37.809330, lng: -122.469835)
+Bench.create!(description: 'Fort Mason', lat: 37.810618, lng: -122.424431)

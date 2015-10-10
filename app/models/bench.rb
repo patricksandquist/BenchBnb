ff0f84a62d055962a32644a98884090c4d86f8b2
patricksandquist <patricksandquist@gmail.com>
2015-10-09 17:07:03 -0700

    Only load markers that are in the boundaries of the map

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -9,7 +9,16 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+require 'json'
 
 class Bench < ActiveRecord::Base
   validates :description, :lat, :lng, presence: true
+  def self.in_bounds(bounds)
+    neLat = bounds["northEast"]["lat"].to_f
+    neLng = bounds["northEast"]["lng"].to_f
+    swLat = bounds["southWest"]["lat"].to_f
+    swLng = bounds["southWest"]["lng"].to_f
+
+    return Bench.where(lat: swLat..neLat).where(lng: swLng..neLng).to_a
+  end
 end

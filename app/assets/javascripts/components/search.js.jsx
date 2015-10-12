4d725ffb7a9784c5054aef4226bf00141b8b1a79
App Academy Student <student@appacademy.io>
2015-10-12 14:22:01 -0700

    Add FilterParams store and action. Refactor to include

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -1,6 +1,26 @@
-/* global React, Map, BenchesIndex, BenchStore */
+/* global ApiUtil, FilterParamsStore, React, Map, BenchesIndex, BenchStore */
 
 window.Search = React.createClass({
+  getInitialState: function () {
+    return {bounds: {}, minSeating: 0, maxSeating: 999};
+  },
+
+  _onChange: function () {
+    var paramsArray = FilterParamsStore.getParams();
+
+    this.setState({
+      bounds: paramsArray[0],
+      minSeating: paramsArray[1],
+      maxSeating: paramsArray[2]
+    });
+
+    ApiUtil.fetchBenches();
+  },
+
+  componentDidMount: function () {
+    FilterParamsStore.addChangeListener(this._onChange);
+  },
+
   handleMapClick: function (coords) {
     this.props.history.pushState(null, "benches/new", coords);
   },
